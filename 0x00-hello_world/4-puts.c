cc5f61498286ac0a291d9a0cc277c58e75afbcdf
okoliegrace <okoliegrace>
2022-12-12 13:12:01 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <studio.h>
-/*
- * main -print a line of code using puts
+#include <stdio.h>
+/**
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
