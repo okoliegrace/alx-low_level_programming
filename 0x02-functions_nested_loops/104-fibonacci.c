ae4d5f52bb7c5b4b162bc3b2c436492f36455ad0
okoliegrace <okoliegrace>
2022-12-13 18:44:40 -0800

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
-while (i < 98)
+		while (i < 98)
 	{
-	next = a + b;
-	a = b;
-	b = next;
-	printf("%lu", next);
-if (i < 97)
-	printf(", ");
-	i++;
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+	if (i < 97)
+		printf(", ");
+		i++;
 	}
 	putchar('\n');
-return (0);
+	return (0);
 }
